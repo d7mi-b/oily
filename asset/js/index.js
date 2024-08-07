afcdcc2b25474b4262af736a81dc78e1a01e2ecb
Abdulrahman <d7mii.b@gmail.com>
2024-08-07 13:33:48 +0300

    create services, customers, and footer in home page

---
+++ asset/js/index.js
@@ -0,0 +1,110 @@
+import Navbar from '../../components/navbar.js';
+
+Navbar();
+
+const services = [
+    {
+        id: 1,
+        service: "نظام مضخات الحريق",
+        img: "Frame"
+    },
+    {
+        id: 2,
+        service: "نظام انذار الحرائق",
+        img: "Frame-2"
+    },
+    {
+        id: 3,
+        service: "نظام مكافحة الحرائق",
+        img: "Group-1"
+    },
+    {
+        id: 4,
+        service: "سيارة إطفاء",
+        img: "Frame-3"
+    },
+    {
+        id: 5,
+        service: "السلامة البحرية",
+        img: "Group 1875"
+    },
+    {
+        id: 6,
+        service: "معدات السلامة",
+        img: "Frame-1"
+    },
+    {
+        id: 7,
+        service: "الحلول الهندسية",
+        img: "Frame-4"
+    },
+    {
+        id: 8,
+        service: "ادوات المختبر",
+        img: "Group 1876"
+    },
+    {
+        id: 9,
+        service: "نظام المراقة",
+        img: "Group"
+    },
+];
+
+const servicesContainer = document.getElementById('services-container');
+
+services.forEach(e => {
+    servicesContainer.innerHTML += `
+        <article class="service" key=${e.id}>
+            <section class="icon center">
+                <img src="./asset/icons/${e.img}.svg" alt="icon">
+            </section>
+
+            <header>
+                <h3>${e.service}</h3>
+            </header>
+        </article>
+    `;
+});
+
+const customers = [
+    {
+        id: 1,
+        name: "نما",
+        logo: "Gov_Logo-5"
+    },
+    {
+        id: 2,
+        name: "وزارة الصناعة والثروة المعدنية",
+        logo: "Gov_Logo-4"
+    },
+    {
+        id: 3,
+        name: "اعتماد",
+        logo: "Gov_Logo-3"
+    },
+    {
+        id: 4,
+        name: "سكني",
+        logo: "Gov_Logo-2"
+    },
+    {
+        id: 5,
+        name: "ايجار",
+        logo: "Gov_Logo-1"
+    },
+    {
+        id: 6,
+        name: "وزارة الإستثمار",
+        logo: "Gov_Logo"
+    },
+];
+
+const customersContainer = document.getElementById('customers-container');
+
+customers.forEach(e => {
+    customersContainer.innerHTML += `
+        <section class="customer center" title="${e.name}">
+            <img src="./asset/images/${e.logo}.png" alt="Customer's logo">
+        </section>
+    `;
+})
---
+++ asset/js/products.js
@@ -0,0 +1,76 @@
+const products = [
+    {
+        id: 1,
+        product: "خوذة سلامة نظام أمريكي بفتحات تهوئة متوفرة بعدة الوان",
+        img: "Image"
+    },
+    {
+        id: 2,
+        product: "سماعة سلامة من شركة 3M الأمريكية",
+        img: "Image-3"
+    },
+    {
+        id: 3,
+        product: "سترة سلامة أحترافية متينة مصممة بجيبوب وسحاب لون فسفوري",
+        img: "Image-6"
+    },
+    {
+        id: 4,
+        product: "قناع لحام",
+        img: "Image-9"
+    },
+    {
+        id: 5,
+        product: "طفاية حريق بودرة حجم 2 كيلو لمكافحة جميع أنواع الحرائق ",
+        img: "Image-1"
+    },
+    {
+        id: 6,
+        product: "كرة اطفاء الحريق الرهيبة تعمل على اطفاء الحريق فور اندلاعه",
+        img: "Image-4"
+    },
+    {
+        id: 7,
+        product: "قفازات يد منقطّة مبطنة بالقطن PVC",
+        img: "Image-7"
+    },
+    {
+        id: 8,
+        product: "طفاية حريق بودرة 6 كيلو اتوماتيك",
+        img: "Image-10"
+    },
+    {
+        id: 9,
+        product: "صندوق طفاية حريق مناسب للحفاظ على طفاية 6 كيلو بودرة ",
+        img: "Image-2"
+    },
+    {
+        id: 10,
+        product: "كاشف دخان بطارية لا سلكي يصدر صافرات أنذار تنبهية بداية انتشار الحريق",
+        img: "Image-5"
+    },
+    {
+        id: 11,
+        product: "نظارة سلامة من شركة 3M الأمريكية متعددة الاستخدامات ",
+        img: "Image-8"
+    },
+    {
+        id: 12,
+        product: "اضاءة طرق - طاقة شمسية - لونين",
+        img: "Image-11"
+    },
+];
+
+const productsContainer = document.getElementById('products-container');
+
+products.forEach(e => {
+    productsContainer.innerHTML += `
+        <article class="product" key="${e.id}">
+            <section class="img center">
+                <img src="./asset/images/${e.img}.png" alt="product's image">
+            </section>
+
+            <p>${e.product}</p>
+        </article>
+    `;
+})
